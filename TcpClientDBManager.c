b724e81de13d99ba490dbacff387d7ecef61d959
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-03-09 17:58:51 +0900

    Created a new type 'TcpClient' to store connected client info.

--- TcpClientDBManager.c
+++ TcpClientDBManager.c
@@ -7,6 +7,24 @@ search_tcp_client_by_id(void *p, void *key){
     return false;
 }
 
+TcpClient *
+DBM_store_client_conn(int comm_fd, uint32_t ip_addr, uint16_t port_no,
+		      TcpServerController *tsc){
+    TcpClient *client;
+
+    if ((client = (TcpClient *) malloc(sizeof(TcpClient))) == NULL){
+	perror("malloc");
+	exit(-1);
+    }
+
+    client->comm_fd = comm_fd;
+    client->ip_addr = ip_addr;
+    client->port_no = port_no;
+    client->tsc = tsc;
+
+    return NULL;
+}
+
 TcpClientDBManager *
 DBM_create(TcpServerController *tsc){
     TcpClientDBManager *db_manager;
