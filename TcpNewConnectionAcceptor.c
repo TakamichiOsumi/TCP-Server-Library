1eca2e0e5c6dc461c76726ef44afd687be931ae0
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-03-09 12:39:34 +0900

    Make the master socket and utilize it to get communication file descriptor.

--- TcpNewConnectionAcceptor.c
+++ TcpNewConnectionAcceptor.c
@@ -1,7 +1,10 @@
+#include <netinet/in.h>
 #include <pthread.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "TcpNewConnectionAcceptor.h"
 
 /*
@@ -9,7 +12,7 @@
  *
  * Create an master socket file descriptor using socket().
  */
-static int master_fd;
+static int master_socket;
 
 /* Thread to listen for new connection from new client */
 static pthread_t *conn_acceptor;
@@ -24,7 +27,8 @@ CAS_create(TcpServerController *tsc){
 	exit(-1);
     }
 
-    if ((master_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    /* The socket setup will be done in CAS_listen_new_tcp_connection() */
+    if ((master_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1){
 	perror("socket");
 	exit(-1);
     }
@@ -41,8 +45,45 @@ CAS_create(TcpServerController *tsc){
 
 static void *
 CAS_listen_new_tcp_connection(void *arg){
-    TcpNewConnectionAcceptor *cas = arg;
+    TcpNewConnectionAcceptor *cas = (TcpNewConnectionAcceptor *) arg;
+    /* For server bind */
+    struct sockaddr_in tcp_server_addr;
+    /* For connected client socket */
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = (sizeof(client_addr));
+    int comm_socket_fd;
 
+    /* Name the socket created in CAS_create */
+    memset(&tcp_server_addr, 0, sizeof(tcp_server_addr));
+    tcp_server_addr.sin_family = AF_INET; /* IPv4 */
+    tcp_server_addr.sin_port = htons(cas->tsc->port_no);
+    tcp_server_addr.sin_addr.s_addr = htonl(cas->tsc->ip_addr);
+
+    if (bind(master_socket,
+	     (struct sockaddr *) &tcp_server_addr,
+	     sizeof(tcp_server_addr)) == -1){
+	perror("bind");
+	exit(-1);
+    }
+
+    /* Create a queue */
+    if (listen(master_socket, 5) < 0){
+	perror("listen");
+	exit(-1);
+    }
+
+    /*
+     * Now, client can request new connection.
+     */
+
+    while(1){
+	comm_socket_fd = accept(master_socket,
+				(struct sockaddr *) &client_addr,
+				&client_addr_len);
+	fprintf(stderr, "debug : new connection accepted\n");
+    }
+
+    close(master_socket);
 
     return NULL;
 }
@@ -68,8 +109,6 @@ CAS_destroy(TcpNewConnectionAcceptor *cas){
     if (cas == NULL)
 	return;
 
-    close(master_fd);
     free(conn_acceptor);
-    free(acceptor);
     free(cas);
 }
